2e63573b2ae7bf483064dcece7809b7b141ec4fc
rajkris-frondesk <rajkris@frondesk.com>
2025-10-07 12:39:52 +0530

    Updates

--- _next/static/chunks/app/contact/page-1b686345a83dbb0c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977],{6068:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.bind(n,6145))}},e=>{e.O(0,[874,145,441,964,358],()=>e(e.s=6068)),_N_E=e.O()}]);
