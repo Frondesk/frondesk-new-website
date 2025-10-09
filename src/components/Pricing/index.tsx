fbdf26801e198522deafe009b89f92c295b789fd
suwarna0089 <suwarna@frondesk.com>
2025-10-09 23:04:55 +0530

    Comment pricing and subscribe in contact section

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -57,7 +57,7 @@ const Pricing = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
+        {/* <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
           <PricingBox
             packageName="Lite"
             price={isMonthly ? "40" : "120"}
@@ -97,7 +97,7 @@ const Pricing = () => {
             <OfferList text="Lifetime Access" status="active" />
             <OfferList text="Free Lifetime Updates" status="active" />
           </PricingBox>
-        </div>
+        </div> */}
       </div>
 
       <div className="absolute bottom-0 left-0 z-[-1]">
