033cd478aefc19e6850f8061f9a1901822efd0ef
suwarna0089 <suwarna@frondesk.com>
2025-10-30 00:14:47 +0530

    fix for the immage issue

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -67,7 +67,7 @@ const AboutSectionOne = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="relative mx-auto aspect-25/24 max-w-[700px] lg:mr-0">
                 <Image
-                 src={withBasePath('images/about/abo.png')} 
+                 src={withBasePath('/images/about/abo.png')} 
                
                   // src={`${basePath}images/about/abo.png`}
                   //src={getImageSrc('/images/about/about44.png')}
@@ -76,7 +76,7 @@ const AboutSectionOne = () => {
                   className="mx-auto max-w-full drop-shadow-three dark:hidden dark:drop-shadow-none lg:mr-0"
                 />
                 <Image
-               src={withBasePath('images/about/abo.png')} 
+               src={withBasePath('/images/about/abo.png')} 
                   //src={`${basePath}images/about/abo.png`}
                 // src={getImageSrc('/images/about/about44.png')}
                   alt="about-image"
