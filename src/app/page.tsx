e01af5a1b2d86386a740c2bb3278b928c8d16545
suwarna0089 <suwarna@frondesk.com>
2025-11-17 14:30:40 +0530

    fix for build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+export const revalidate = 60;
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Blog from "@/components/Blog";
